ae77d4838eabc1e96d11b7b7e96f2b993fbea8ed
Clerissa <clerissalewis@gmail.com>
2021-06-08 17:53:23 +0400

    Added print statements for pow in the main module

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/team-kodo/golang-blockchain.git/blockchain"
 )
@@ -18,6 +19,10 @@ func main() {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 
 	}
 }
