9e5f85ceb126cb453f4b699706574c9616a4ba88
Clerissa <clerissalewis@gmail.com>
2021-06-08 15:35:11 +0400

    Finished proofOfWork module

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -2,8 +2,11 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/binary"
+	"fmt"
 	"log"
+	"math"
 	"math/big"
 )
 
@@ -37,7 +40,41 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
 	return data
 }
 
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
 
+	nonce := 0
+
+	for nonce < math.MaxInt64 {
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+
+		fmt.Printf("\r%x", hash)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+
+	}
+	fmt.Println()
+
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := pow.InitData(pow.Block.Nonce)
+
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
 
 func ToHex(num int64) []byte {
 	buff := new(bytes.Buffer)
