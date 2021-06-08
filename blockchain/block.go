344bb0aa0d74ca348276fc64c9535342960ff78d
Clerissa <clerissalewis@gmail.com>
2021-06-08 17:52:29 +0400

    Proof of work implementation

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,42 +1,37 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
-type BlockChain struct{
+type BlockChain struct {
 	Blocks []*Block
 }
 
-type Block struct{
-	Hash [] byte
-	Data [] byte
-	PrevHash [] byte
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+	Nonce    int
 }
 
-func (b *Block) DeriveHash(){
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
 
-func CreateBlock(data string, prevHash []byte) *Block{
-	block:= &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string){
+func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, new)
 }
 
-func Genesis() *Block{
+func Genesis() *Block {
 	return CreateBlock("Team Kodo's genesis block", []byte{})
 }
 
-func InitBlockChain() *BlockChain{
+func InitBlockChain() *BlockChain {
 	return &BlockChain{[]*Block{Genesis()}}
 }
