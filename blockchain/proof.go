f192b76c2e9a45bc0cbf07cffa8462109be528ce
Clerissa <clerissalewis@gmail.com>
2021-06-08 15:34:32 +0400

    Added toHex, newProof functions

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,6 +1,9 @@
 package blockchain
 
 import (
+	"bytes"
+	"encoding/binary"
+	"log"
 	"math/big"
 )
 
@@ -11,3 +14,38 @@ type ProofOfWork struct {
 	Target *big.Int
 }
 
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+
+	}
+
+	return buff.Bytes()
+}
